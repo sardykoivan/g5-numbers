67abd74b8951b0f413c16bb1db492946daf26494
Ivan Sardyko <sardyco@gmail.com>
2022-09-23 08:49:28 +0300

    up

--- public/index.php
+++ public/index.php
@@ -9,8 +9,42 @@ use Ivan\G5Numbers\HappyTicket;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$happyTicketCalculator = new HappyTicketCalculator();
+//$happyTicketCalculator = new HappyTicketCalculator();
+//
+//$b = $happyTicketCalculator->calculateHappyTicketsCount('000000', '999999');
+//
+//var_dump($b);die();
 
-$b = $happyTicketCalculator->calculateHappyTicketsCount('000000', '999999');
+// преобразовать входные параметры в инт чтобы можно было итерировать
+// собрать массив обратно, заполнив его в формат как было в запросе
+// посчитать количество счастливых билетов
 
-var_dump($b);die();
+ $first = '000000';
+ $end = '999999';
+
+function intToStr(int $int) {
+    return $str = substr('00000' . $int, -6);
+}
+
+function strToInt(string $string) {
+    return (int) $string;
+}
+
+$lucky = array();
+for($i = strToint($first); $i <= strToInt($end); $i++){
+    $str = intToStr($i);
+    if($str[0] + $str[1] + $str[2] == $str[3] + $str[4] + $str[5]){
+        $lucky[] = $str;
+    }
+}
+echo count($lucky);
+
+
+//$lucky = array();
+//for($i = 0; $i < 1000000; $i++){
+//    $str = substr('00000' . $i, -6);
+//    if($str[0] + $str[1] + $str[2] == $str[3] + $str[4] + $str[5]){
+//        $lucky[] = $str;
+//    }
+//}
+//echo count($lucky);
